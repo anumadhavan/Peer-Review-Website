ec13071a61cba8f7225bd889d71d906d69547960
Anu Madhavan <anumadhavan1998@gmail.com>
2025-04-15 13:51:19 +0530

    Update

--- peerReview_frondend/src/app/auth/service.service.ts
+++ peerReview_frondend/src/app/auth/service.service.ts
@@ -1,38 +1,3 @@
-/*import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ServiceService {
-
-  private role: 'admin' |'employee' |null = null;
-
-  constructor(private router:Router ) { }
-
-  setRole(role:'admin' | 'employee'){
-    this.role = role;
-    localStorage.setItem('userRole',role);
-  }
-
-  getRole():'admin' |'employee' | null {
-    if(!this.role){
-      this.role= localStorage.getItem('userRole') as 'admin' |'employee' |null;
-    }
-    return this.role;
-  }
-
-  isLoggedIn():boolean{
-    return !! this.getRole();
-  }
-
-  logout(){
-    this.role = null;
-    localStorage.removeItem('userRole');
-    this.router.navigate(['/login']);
-  }
-}*/
-
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
